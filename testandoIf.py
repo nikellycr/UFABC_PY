b2eb3187254594777bea78a0029e9c512b13b155
Nikelly Carvalho <45895531+nikellycr@users.noreply.github.com>
2018-12-27 19:42:13 -0200

    Create testandoIf.py

---
+++ testandoIf.py
@@ -0,0 +1,10 @@
+x=int(input("Sua idade: "))
+
+if x >= 18: 
+  print("Você já pode dirigir")
+
+elif x >= 16:
+  print("Você já pode votar mas não dirigir")
+
+else:
+  print("Você não pode nem dirigir e nem votar")
